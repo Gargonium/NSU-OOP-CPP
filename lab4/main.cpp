499d1e933adbcf5d9fbae478614f61b79aafd617
Gargonium <tetatet.nikvot@gmail.com>
2023-12-17 22:44:18 +0700

    1 подзадача, 1 и 2 допы к ней

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
-
 #include <tuple>
+#include <utility>
 
+
+/* C ограничением С++11 */
 template<std::size_t index = 0, typename... Types>
 typename std::enable_if<index == sizeof...(Types)>::type
-printTuple(std::ostream&, const std::tuple<Types...>&) {
+printTuple11(std::ostream&, const std::tuple<Types...>&) {
 
 }
 
 template<std::size_t index = 0, typename... Types>
 typename std::enable_if<(index < sizeof...(Types))>::type
-    printTuple(std::ostream& os, const std::tuple<Types...>& t) {
+printTuple11(std::ostream& os, const std::tuple<Types...>& t) {
     if (index != 0) {
         os << ", ";
     }
     os << std::get<index>(t);
-    printTuple<index + 1>(os, t);
+    printTuple11<index + 1>(os, t);
 }
 
+/* Без ограничения */
+/* Используя constexpr */
+template<std::size_t Index = 0, typename... Types>
+std::ostream& printTupleConstExpr(std::ostream& os, const std::tuple<Types...>& t) {
+    if constexpr (Index < sizeof...(Types)) {
+        if (Index != 0)
+            os << ", ";
+        os << std::get<Index>(t);
+        printTupleConstExpr<Index + 1>(os, t);
+    }
+    return os;
+}
+
+/* Используя fold expressions */
+template<typename... Types, std::size_t... Indices>
+void printTupleFold(std::ostream& os, const std::tuple<Types...>& t, std::index_sequence<Indices...>) {
+    ((os << (Indices == 0 ? "" : ", ") << std::get<Indices>(t)), ...);
+}
+
+
+/* Перегрузка оператора << */
 template<typename... Types>
 std::ostream& operator<<(std::ostream& os, const std::tuple<Types...>& t) {
-    printTuple(os, t);
+    printTupleFold(os, t, std::make_index_sequence<sizeof... (Types)>());
     return os;
 }
 
